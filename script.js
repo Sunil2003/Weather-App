d72b9fe67cbaab5ee6b8303dde46202c7fe865d8
Sunil Bansiwal <92246381+Sunil2003@users.noreply.github.com>
2024-03-11 00:00:11 +0530

    Add files via upload
    add weather App files

---
+++ script.js
@@ -0,0 +1,76 @@
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_icon = document.querySelector(".weather_icon");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
+
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
+
+let citySearch = document.querySelector(".weather_search");
+
+
+let city = "pune";
+citySearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let cityName = document.querySelector(".city_name");
+    city = cityName.value;
+
+    getWeatherData();
+
+    cityName.value = "";
+})
+
+
+const getCountryName = (code) => {
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
+};
+
+const getCurrentDateTime = (dt) => {
+  let currDate = new Date(dt * 1000);
+
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("en-US", options).format(currDate);
+};
+
+const getWeatherData = async () => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d9b14121d978f504a9e5d1da5d7a6f2e`;
+
+  try {
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+    console.log(data);
+
+    const { main, name, weather, wind, sys, dt } = data;
+
+    cityName.innerHTML = `${name}, ${getCountryName(sys.country)}`;
+    dateTime.innerHTML = `${getCurrentDateTime(dt)}`;
+    w_forecast.innerHTML = weather[0].main;
+    w_icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`;
+    w_temperature.innerHTML = `${(main.temp - 273.15).toFixed(0)}&#176C`;
+    w_minTem.innerHTML = `MIN : ${(main.temp_min - 273.15).toFixed(2)}&#176C`;
+
+    w_maxTem.innerHTML = `MAX : ${(main.temp_max-273.15).toFixed(2)}&#176C`;
+    w_feelsLike.innerHTML = `${(main.feels_like - 273.15).toFixed(2)}&#176C`;
+    w_humidity.innerHTML = `${main.humidity}%`;
+    w_wind.innerHTML = `${wind.speed} Km/h`;
+    w_pressure.innerHTML = `${main.pressure} hPa`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+document.body.addEventListener("load", getWeatherData());
